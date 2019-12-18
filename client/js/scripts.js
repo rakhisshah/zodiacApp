cf0b56a433637057513d09a7f576f4fca9e43ee4
Rakhi Shah <rakhisshah@gmail.com>
2019-12-18 11:56:36 +0100

    few changes made

---
+++ client/js/scripts.js
@@ -0,0 +1,66 @@
+var zodiacs=[
+{
+	name: "Aries",
+	month: "March 21-April 20",
+	description: "As the first sign in the zodiac, the presence of Aries always marks the beginning of something energetic and turbulent. ... Thanks to its ruling planet Mars and the fact it belongs to the element of Fire (just like Leo and Sagittarius), Aries is one of the most active zodiac signs."
+},
+{
+	name: "Taurus",
+	month: "April 21-May 20",
+	description: "Taurus is an earth sign represented by the bull. ... Taurus is ruled by Venus, the enchanting planet that governs love, beauty, and money. Taurus's Venusian influence make this earth sign the most sensual of the zodiac: These cosmic oxen are enchanted by any physical manifestation of comfort and luxury."
+},
+{
+	name: "Gemini",
+	month: "May 21-June 20",
+	description: "While most signs have one very prominent trait (for example, Capricorns are notoriously ambitious), Geminis, those born between May 21 and June 21, can be social, talkative, and whimsical, but they can also be indecisive or nosy. Some Geminis love to be the center of attention; others love to gossip."
+},
+{
+	name: "Cancer",
+	month: "June 21-Juyy 22",
+	description: "Cancer (♋️) is the fourth astrological sign in the Zodiac, originating from the constellation of Cancer. It spans from 90° to 120° celestial longitude. ... In astrology, Cancer is the cardinal sign of the Water trigon, which is made up of Cancer, Pisces, and Scorpio. It is one of the six negative signs."
+
+},
+{
+	name: "Leo",
+	month: "July23-August 22",
+	description: "Leo (♌) (Greek: Λέων, Leōn), is the fifth astrological sign of the zodiac. ... Under the tropical zodiac, the Sun transits this area on average between July 23 and August 22 each year, and under the sidereal zodiac, the Sun currently transits this area from approximately August 16 to September 15."
+},
+
+
+{
+	name: "Virgo",
+	month: "August 23-September 22",
+	description: "Virgo (♍) (Greek: Παρθένος, Parthenos) is the sixth astrological sign in the Zodiac. ... Under the tropical zodiac, the Sun transits this area on average between August 23 and September 22, and the Sun transits the constellation of Virgo from approximately September 16 to October 30.
+	Zodiac element: Earth"
+},
+{
+	name: "Libra",
+	month: "September 23-october 22",
+	description: "Libra is an air sign represented by the scales (interestingly, the only inanimate object of the zodiac), an association that reflects Libra's fixation on balance and harmony. Libra is obsessed with symmetry and strives to create equilibrium in all areas of life."
+},
+{
+	name: "Scorpio",
+	month: "October 23- November 22 ",
+	description: "Scorpio is one of the most misunderstood signs of the zodiac. ... In fact, Scorpio is a water sign that derives its strength from the psychic, emotional realm. Like its fellow water signs, Cancer and Pisces, Scorpio is extremely clairvoyant and intuitive."
+},
+{
+	name: "Sagittarius",
+	month: "November 23- Deccember 21",
+	description: "These zodiac "cuspers" are bold, passionan of you were born between Nov. 18 and 24, you fall in the Scorpio-Sagittarius cusp, also called the Cusp of Revolution. You are known as a Scorpio-Sagittarius cusper. If you have any planet on the cusp, you show traits of both zodiac signs."
+},
+{
+	name: "Capricorn",
+	month: "Deccember 22- January 21"
+	description: "Scorpio and Capricorn share a special sexual bond as signs in sextile with each other and .... Scorpio woman - information and insights on the Scorpio woman."
+},
+{
+	name: "Aquarius",
+	month: "January 22- February 22",
+	description: "If you're friends with an Aquarius or Pisces, you're already familiar with their most prominent quality. They are very compassionate and calm. They don't let their emotions rule them. ... Being born between Aquarius and Pisces implies dealing with so many mixed feelings."
+},
+{
+	name: "Pisces",
+	month: "February 23- March 21",
+	description: "Pisces is a sensitive water sign, ruled by the planet Neptune. Your sign is fluid and tends to be sympathetic to the underdog. It's so easy for you to feel other people's experiences that they can obscure your own. You can be vague and escapist, but make up for it by being compassionate and intuitive"
+}
+]
